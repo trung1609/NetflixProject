4b9fa06347aeccda29b7bb20233e35d72dde1c5e
trung1609 <vmt162005@gmail.com>
2026-02-13 19:56:43 +0700

    initialize project structure with Gradle and Spring Boot setup

---
+++ NetflixBackend/src/main/java/com/api/netflixbackend/NetflixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.api.netflixbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixBackendApplication.class, args);
+    }
+
+}
