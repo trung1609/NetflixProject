268b49ec4a9f9190c2b9af43c9a8c53a72f389c0
trung1609 <vmt162005@gmail.com>
2026-02-20 08:38:03 +0700

    add user management features including pagination, user deletion, status toggling, and role changes

--- NetflixBackend/src/main/java/com/api/netflixbackend/repository/UserRepository.java
+++ NetflixBackend/src/main/java/com/api/netflixbackend/repository/UserRepository.java
@@ -2,7 +2,11 @@ package com.api.netflixbackend.repository;
 
 import com.api.netflixbackend.entity.User;
 import com.api.netflixbackend.enums.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -20,4 +24,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 
     long countByRoleAndActive(Role role, boolean active);
+
+    @Query("select u from User u where " +
+            "lower(u.fullName) like lower(concat('%', :search, '%')) or " +
+            "lower(u.email) like lower(concat('%', :search, '%') ) ")
+    Page<User> searchUsers(@Param("search") String search, Pageable pageable);
+
+    long countByRole(Role role);
 }
