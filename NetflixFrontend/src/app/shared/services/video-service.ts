ee7778f060801f6defe21153dba5e988357b8627
trung1609 <vmt162005@gmail.com>
2026-02-23 20:09:44 +0700

    feat: add MediaService and VideoService with corresponding methods for file and video management

---
+++ NetflixFrontend/src/app/shared/services/video-service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { REACTIVE_NODE } from '@angular/core/primitives/signals';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VideoService {
+  private apiUrl = environment.apiUrl + '/videos';
+  private apiUrlAdmin = environment.apiUrl + '/videos/admin';
+
+  constructor(private http: HttpClient) {}
+
+  getAllAdminVideos(page: number, size: number, search?: string) {
+    let params = new HttpParams().set('page', page).set('size', size);
+
+    if (search) {
+      params = params.set('search', search);
+    }
+    return this.http.get(this.apiUrlAdmin, { params });
+  }
+
+  createVideoByAdmin(data: any) {
+    return this.http.post(this.apiUrlAdmin, data);
+  }
+
+  updateVideoByAdmin(id: string | number, data: any) {
+    return this.http.put(this.apiUrlAdmin + '/' + id, data);
+  }
+
+  deleteVideoByAdmin(id: string | number) {
+    return this.http.delete(this.apiUrlAdmin + '/' + id);
+  }
+
+  setPublishedByAdmin(id: string | number, published: boolean) {
+    return this.http.patch(this.apiUrlAdmin + '/' + id + '/publish?value=' + published, {});
+  }
+
+  getStatsByAdmin() {
+    return this.http.get(this.apiUrlAdmin + '/stats');
+  }
+}
