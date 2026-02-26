1f903e949190160c967de2326e249b7c5a21cc96
trung1609 <vmt162005@gmail.com>
2026-02-26 09:54:23 +0700

    feat: increase file upload size limit to 10GB and implement watchlist service

---
+++ NetflixFrontend/src/app/shared/services/watchlist-service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WatchlistService {
+  private apiUrl = environment.apiUrl + '/watchlist';
+  constructor(private http: HttpClient) { }
+  getWatchlist(page: number = 0, size: number = 10, search: string) {
+    let params = new HttpParams()
+      .set('page', page)
+      .set('size', size);
+
+    if (search) {
+      params = params.set('search', search);
+    }
+    return this.http.get(this.apiUrl, { params });
+  }
+
+  addToWatchlist(videoId: number | string) {
+    return this.http.post(this.apiUrl + '/' + videoId, {});
+  }
+
+  removeFromWatchlist(videoId: number | string) {
+    return this.http.delete(this.apiUrl + '/' + videoId);
+  }
+}
--- NetflixFrontend/src/app/user/home/home.ts
+++ NetflixFrontend/src/app/user/home/home.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
+import { VideoService } from '../../shared/services/video-service';
+import { WatchlistService } from '../../shared/services/watchlist-service';
+import { NotificationService } from '../../shared/services/notification-service';
+import { UtilityService } from '../../shared/services/utility-service';
+import { MediaService } from '../../shared/services/media-service';
+import { DialogService } from '../../shared/services/dialog-service';
+import { ErrorHandlerService } from '../../shared/services/error-handler-service';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +14,82 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements OnInit, OnDestroy {
+  allVideos: any = [];
+  filteredVideos: any = [];
+  loading = true;
+  loadingMore = false;
+  error = false;
+  searchQuery: string = '';
+
+  featuredVideos: any[] = [];
+  currentSlideIndex = 0;
+  featuredLoading = true;
+
+  currentPage = 0;
+  pageSize = 10;
+  totalElements = 0;
+  totalPages = 0;
+  hasMoreVideos = true;
+
+  private searchSubject = new Subject<String>();
+
+  private sliderInterval: any;
+  private savedScrollPosition: number = 0;
+
+  constructor(
+    private videoService: VideoService,
+    private watchlistService: WatchlistService,
+    private notification: NotificationService,
+    public utilityService: UtilityService,
+    public mediaService: MediaService,
+    private dialogService: DialogService,
+    private errorHandlerService: ErrorHandlerService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.loadFeaturedVideos();
+    this.loadVideos();
+    this.initializeSearchDebounce();
+  }
+
+  ngOnDestroy(): void {
+    this.searchSubject.complete();
+  }
+
+  initializeSearchDebounce(): void {
+    this.searchSubject.pipe(
+      debounceTime(500),
+      distinctUntilChanged()
+    ).subscribe(() => {
+      this.preformSearch();
+    });
+  }
+
+  loadFeaturedVideos() {
+    this.featuredLoading = true;
+    this.videoService.getFeaturedVideos().subscribe({
+      next: (videos: any) => {
+        this.featuredVideos = videos;
+        this.featuredLoading = false;
+        if (this.featuredVideos.length > 1) {
+          this.startSlider();
+        }
+      },
+      error: (err) => {
+        this.featuredLoading = false;
+        this.errorHandlerService.handle(err, 'Failed to load featured videos');
+      }
+    });
+  }
+  private startSlider() { }
+
+  loadVideos() { }
+
+  private preformSearch() { }
+
+  stopSlider() { }
 
 }
